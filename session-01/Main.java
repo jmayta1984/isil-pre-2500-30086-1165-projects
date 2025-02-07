eab4f21249221c89b895079d08684327b516937e
Jorge M <38768001+jmayta1984@users.noreply.github.com>
2025-02-06 19:12:08 -0500

    Add files via upload

---
+++ session-01/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+
+    public static void method(){
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, ISIL");
+        
+        byte age = 40;
+        System.out.println(age);
+        
+        String name = "Jorge";
+        System.out.println(name);
+
+        int result = 12 / 5;
+        System.out.println(result);
+
+
+        System.out.println(13 % 5);
+    
+        System.out.println(13 < 5);
+    }
+
+}
