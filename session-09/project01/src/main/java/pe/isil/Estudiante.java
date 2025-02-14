e1611e81ef5c01c55080f33ba761665aba713d15
jmayta1984 <jmayta@pucp.edu.pe>
2025-02-13 19:42:46 -0500

    chore: update project

--- session-09/project01/src/main/java/pe/isil/Estudiante.java
+++ session-09/project01/src/main/java/pe/isil/Estudiante.java
@@ -1,12 +1,12 @@
 package pe.isil;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Estudiante {
     private String nombre;
     private int edad;
     private ArrayList<Curso> cursos;
-    
 
     /*
      * Constructor
@@ -26,32 +26,53 @@ public class Estudiante {
         return nombre;
     }
 
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
 
     public int getEdad() {
         return edad;
     }
 
-
     public void setEdad(int edad) {
         this.edad = edad;
     }
-
 
     public ArrayList<Curso> getCursos() {
         return cursos;
     }
 
-
     public void setCursos(ArrayList<Curso> cursos) {
         this.cursos = cursos;
     }
-     
 
-    
-    
+    public void inscribirCurso(Curso curso) {
+        cursos.add(curso);
+    }
+
+    public void registrarNotasFinales() {
+        Scanner scanner = new Scanner(System.in);
+        for (Curso curso : cursos) {
+            System.out.println("Ingrese la nota final del curso " + curso.getNombre() + ":");
+            double nota = scanner.nextDouble();
+            curso.setNota(nota);
+        }
+        scanner.close();
+    }
+
+    public double calcularPonderado() {
+        double total = 0;
+        double creditos = 0;
+
+        for (Curso curso : cursos) {
+            total += curso.getNota() * curso.getCreditos();
+            creditos += curso.getCreditos();
+        }
+
+        return total/creditos;
+    }
+
+    public void imprimirPonderado() {
+        System.out.println("El ponderado final es: " + calcularPonderado());
+    }
 }
--- session-09/project01/src/main/java/pe/isil/Main.java
+++ session-09/project01/src/main/java/pe/isil/Main.java
@@ -1,7 +1,43 @@
 package pe.isil;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner  = new Scanner(System.in);
+        System.out.println("Ingrese los datos del estudiante");
+
+        System.out.println("Nombre: ");
+        String nombre = scanner.next();
+        
+        System.out.println("Edad: ");
+        int edad = scanner.nextInt();
+
+        Estudiante estudiante = new Estudiante(nombre, edad);
+
+        System.out.println("Ingrese la cantidad de cursos");
+        int cantidad = scanner.nextInt();
+
+        for (int i = 0; i < cantidad; i++){
+            System.out.println("Código de curso: ");
+            String codigoCurso = scanner.next();
+
+            System.out.println("Nombre de curso: ");
+            String nombreCurso = scanner.next();
+
+            System.out.println("Cantidad de créditos: ");
+            double creditosCurso = scanner.nextDouble();
+            
+            Curso curso = new Curso(nombreCurso, codigoCurso, creditosCurso);
+            
+            estudiante.inscribirCurso(curso);
+
+        }
+        scanner.close();
+
+        estudiante.registrarNotasFinales();
+
+        estudiante.imprimirPonderado();
+
     }
 }
