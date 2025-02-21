b9b2d81b972d8336ab7f57c4cfedffe74797251f
jmayta1984 <jmayta@pucp.edu.pe>
2025-02-20 19:58:03 -0500

    chore: initial project setup

---
+++ session-13/project01/src/main/java/pe/isil/Estudiante.java
@@ -0,0 +1,28 @@
+package pe.isil;
+
+public class Estudiante extends Persona {
+
+    private String carrera;
+
+    public Estudiante(String nombre, int edad, String carrera) {
+        super(nombre, edad);
+        this.carrera = carrera;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public void estudiar() {
+        System.out.println(getNombre() + " estudia la carrera de " + carrera);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " carrera-" + carrera;
+    }
+}
